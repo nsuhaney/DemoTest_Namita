cc6fb55390156bedc8b58935440804619d914f12
nsuhaney <namita.suhaney@arq.group>
2022-08-17 12:06:07 +1000

    Added Belong coding test

---
+++ src/test/java/autoapp/automation/runner/TestRunner.java
@@ -0,0 +1,15 @@
+package autoapp.automation.runner;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+				features = "src/test/java/autoapp/automation/features", 
+				glue = { "autoapp/automation/stepDef","autoapp/automation/utility" }, 
+				plugin = { "pretty", "html:target/cucumber-html-report", "json:cucumber.json" }
+				)
+
+public class TestRunner {
+}
